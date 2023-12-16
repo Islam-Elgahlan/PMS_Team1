23741feac885110cc9952f319e534b7d706905b8
Salhoom75 <salhoomadel@gmail.com>
2023-12-16 20:58:07 +0200

    login-ui

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -3,14 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-
 export class LoginComponent {
-foods: any[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
+  hide: boolean = true;
+  foods: any[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
   ];
 }
- 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SharedRoutingModule } from './shared-routing.module';
 import { MaterialModule } from './material/material/material.module';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxDropzoneModule } from 'ngx-dropzone';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
@@ -16,15 +13,7 @@ import { MatInputModule } from '@angular/material/input';
     MaterialModule,
     ToastrModule,
     NgxDropzoneModule,
-    MatFormFieldModule,
-    MatInputModule,
   ],
-  exports: [
-    MaterialModule,
-    ToastrModule,
-    NgxDropzoneModule,
-    MatFormFieldModule,
-    MatInputModule,
-  ],
+  exports: [MaterialModule, ToastrModule, NgxDropzoneModule],
 })
 export class SharedModule {}
