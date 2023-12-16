33d492ef322a6a07d9771393a3649192ddf702c1
EmanMohamed22 <emyelgarf22@gmail.com>
2023-12-16 13:51:20 +0200

    eman

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RequestChangePasswordComponent } from './components/request-change-password/request-change-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
 
 const routes: Routes = [
   {path:'' , redirectTo:'login' , pathMatch:'full'},
   {path:'login' , component:LoginComponent},
+  {path:'request-change' , component:RequestChangePasswordComponent},
+  {path:'reset-password' , component:ResetPasswordComponent},
+  {path:'change-password' , component:ChangePasswordComponent},
 
 ];
 
---
+++ src/app/auth/components/request-change-password/request-change-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-change-password',
+  templateUrl: './request-change-password.component.html',
+  styleUrls: ['./request-change-password.component.scss']
+})
+export class RequestChangePasswordComponent {
+
+}
