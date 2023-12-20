4b11d91c5b9828b6fead412b7916ce93f8639500
Islam-Elgahlan <islam.elgahlan@gmail.com>
2023-12-20 02:38:55 -0800

    structure solved

--- src/app/manager/manager-routing.module.ts
+++ src/app/manager/manager-routing.module.ts
@@ -4,7 +4,12 @@ import { ManagerComponent } from './components/manager/manager.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  
+
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'projects',loadChildren: () => import('./manager-projects/manager-projects.module').then(m => m.ManagerProjectsModule)},
+  {path:'tasks',loadChildren: () => import('./manager-tasks/manager-tasks.module').then(m => m.ManagerTasksModule)}
+
 ];
 
 @NgModule({
