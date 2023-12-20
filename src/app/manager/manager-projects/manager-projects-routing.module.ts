a9c42a4d341034124934c0cc8e969b0f280ed04a
Islam-Elgahlan <islam.elgahlan@gmail.com>
2023-12-20 02:36:33 -0800

    structure solved

--- src/app/manager/manager-projects/manager-projects-routing.module.ts
+++ src/app/manager/manager-projects/manager-projects-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddNewProjectComponent } from './components/add-new-project/add-new-project.component';
+import { ManagerComponent } from '../components/manager/manager.component';
 
 const routes: Routes = [
-  
+  {path:'',component:ManagerComponent},
   {path:'add-new-project' , component:AddNewProjectComponent},
 ];
 
--- src/app/manager/manager-routing.module.ts
+++ src/app/manager/manager-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManagerComponent } from './components/manager/manager.component';
+import { HomeComponent } from './components/home/home.component';
 
  
 const routes: Routes = [
-  {path:'' , component:ManagerComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'projects',loadChildren: () => import('./manager-projects/manager-projects.module').then(m => m.ManagerProjectsModule)},
+  {path:'tasks',loadChildren: () => import('./manager-tasks/manager-tasks.module').then(m => m.ManagerTasksModule)}
 ];
 
 @NgModule({
