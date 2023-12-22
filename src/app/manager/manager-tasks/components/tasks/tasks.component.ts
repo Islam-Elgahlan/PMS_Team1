11e2b83699c61dbbbb116c00c7a872c01ab521f1
Salhoom75 <salhoomadel@gmail.com>
2023-12-22 20:56:36 +0200

    tasks-table

--- src/app/manager/manager-tasks/components/tasks/tasks.component.ts
+++ src/app/manager/manager-tasks/components/tasks/tasks.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { ITask } from 'src/app/models/project';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.scss'],
 })
 export class TasksComponent {
-
+  tasksList: ITask[] = [];
+  constructor(private _TaskService: TaskService) {}
+  ngOnInit() {
+    this.openTasks();
+  }
+  openTasks() {
+    this._TaskService.getAllTasks().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.tasksList = res;
+      },
+      error: (err) => {},
+      complete: () => {},
+    });
+  }
 }
