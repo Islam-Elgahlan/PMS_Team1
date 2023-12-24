400324e7fb2588c0af8a45c5a386e52f9346281b
Salhoom75 <salhoomadel@gmail.com>
2023-12-24 20:02:38 +0200

    block-user

--- src/app/manager/manager-users/components/users/users.component.ts
+++ src/app/manager/manager-users/components/users/users.component.ts
@@ -3,56 +3,85 @@ import { IEmployee, ITable } from 'src/app/models/project';
 import { HelperService } from 'src/app/services/helper.service';
 import { UserService } from '../../services/user.service';
 import { PageEvent } from '@angular/material/paginator';
+import { MatDialog } from '@angular/material/dialog';
+import { BlockUserComponent } from './block-user/block-user.component';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent implements OnInit{
-  tableData:IEmployee[]=[]
-constructor(private _user:UserService){
-
-}
-searchValue:string=''
-pageSize:number=200;
-pageNumber:number=1;
-tableRes:ITable | any;
+export class UsersComponent implements OnInit {
+  tableData: IEmployee[] = [];
+  constructor(
+    private _user: UserService,
+    public dialog: MatDialog,
+    private _ToastrService: ToastrService
+  ) {}
+  searchValue: string = '';
+  pageSize: number = 200;
+  pageNumber: number = 1;
+  tableRes: ITable | any;
   ngOnInit(): void {
-    this.onGetAllUsers()
+    this.onGetAllUsers();
   }
 
-
-onGetAllUsers(){
-  let params={
-pageSize:this.pageSize,
-pageNumber:this.pageNumber,
-userName:this.searchValue
+  onGetAllUsers() {
+    let params = {
+      pageSize: this.pageSize,
+      pageNumber: this.pageNumber,
+      userName: this.searchValue,
+    };
+    this._user.getAllUsers(params).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.tableRes = res;
+        this.tableData = res.data;
+      },
+      error: (err) => {},
+      complete: () => {},
+    });
   }
-  this._user.getAllUsers(params).subscribe({
-    next:(res)=>{
-      console.log(res);
-      this.tableRes=res
-      this.tableData=res.data
-    },error:(err)=>{
 
-    },complete:()=> {
-      
-    },
-  })
-}
+  search() {
+    console.log(this.searchValue);
+    this.onGetAllUsers();
+  }
 
-search(){
-  console.log(this.searchValue);
-  this.onGetAllUsers()
-}
+  handlePageEvent(e: PageEvent) {
+    console.log(e);
+    this.pageSize = e.pageSize;
+    // this.pageNumber=this.tableRes.pageNumber;
+    this.pageNumber = e.pageIndex;
 
-handlePageEvent(e:PageEvent){
-  console.log(e);
-  this.pageSize=e.pageSize;
-  // this.pageNumber=this.tableRes.pageNumber;
-  this.pageNumber=e.pageIndex;
-  
-  this.onGetAllUsers()
+    this.onGetAllUsers();
+  }
+  openBlockDialog(item: IEmployee) {
+    const dialogRef = this.dialog.open(BlockUserComponent, {
+      data: item,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed', result);
+      if (result) {
+        this.onBlockUser(result);
+      }
+    });
+  }
+
+  onBlockUser(id: number) {
+    this._user.onBlockOrUnblockUser(id).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        this._ToastrService.error('Can’tBlock this User', 'Error');
+      },
+      complete: () => {
+        this._ToastrService.success('This user was blocked Successfully', 'Done');
+        this.onGetAllUsers();
+      },
+    });
   }
 }
