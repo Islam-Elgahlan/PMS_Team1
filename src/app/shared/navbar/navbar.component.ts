a301296c717c7db78e0b937ea883cd451a4ef73a
Salhoom75 <salhoomadel@gmail.com>
2024-01-01 14:24:09 +0200

    test

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -8,61 +8,51 @@ import { LogOutComponent } from './log-out/log-out.component';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  role :string |undefined = localStorage.getItem('role')?.toLowerCase();
-  constructor(private _Router: Router, private _HelperService: HelperService,
-    public dialog: MatDialog) {
-
-  }
+  role: string | undefined = localStorage.getItem('role')?.toLowerCase();
+  constructor(
+    private _Router: Router,
+    private _HelperService: HelperService,
+    public dialog: MatDialog
+  ) {}
   ngOnInit() {
-    this.onGetCurrentUser()
+    this.onGetCurrentUser();
   }
   logOut() {
-    const dialogRef = this.dialog.open(LogOutComponent, {
-      
-    });
-
-    
-   
+    const dialogRef = this.dialog.open(LogOutComponent);
   }
-  
-  
-  currentUser :any;
+
+  currentUser: any;
   onGetCurrentUser() {
     this._HelperService.getCurrentUser().subscribe((res) => {
-      
       this.currentUser = res;
-     
-    })
+    });
   }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(ChangePasswordComponent, {
       data: {},
-      width:'40%'
+      width: '40%',
     });
-  
-    dialogRef.afterClosed().subscribe(result => {
-     
+
+    dialogRef.afterClosed().subscribe((result) => {
       // if(result){
       //   this.onRequestRestPassword(result)
       // }
-      
     });
   }
-//   openBlockDialog(item: IEmployee) {
-//     const dialogRef = this.dialog.open(BlockUserComponent, {
-//       data: item,
-//     });
+  //   openBlockDialog(item: IEmployee) {
+  //     const dialogRef = this.dialog.open(BlockUserComponent, {
+  //       data: item,
+  //     });
 
-//     dialogRef.afterClosed().subscribe((result) => {
-//       console.log('The dialog was closed', result);
-//       if (result) {
-//         this.onBlockUser(result);
-//       }
-//     });
-//   }
+  //     dialogRef.afterClosed().subscribe((result) => {
+  //       console.log('The dialog was closed', result);
+  //       if (result) {
+  //         this.onBlockUser(result);
+  //       }
+  //     });
+  //   }
 }
-
