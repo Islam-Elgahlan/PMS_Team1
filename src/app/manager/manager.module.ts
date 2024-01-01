b32739b3c5d4600733b1363cc07b44c9a26fcaac
Islam-Elgahlan <islam.elgahlan@gmail.com>
2024-01-01 09:27:10 +0200

    clear

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -5,15 +5,12 @@ import { ManagerRoutingModule } from './manager-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home/home.component';
 
-import { ManagerComponent } from './components/manager/manager.component';
 
 
 
 @NgModule({
   declarations: [
 
-
-    ManagerComponent,
     HomeComponent
   ],
   imports: [
