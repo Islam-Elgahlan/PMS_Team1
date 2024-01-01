09df9ec9afec213ed66a9a36ca049ce232c41d0a
Salhoom75 <salhoomadel@gmail.com>
2024-01-01 14:08:15 +0200

    .

--- src/app/shared/navbar/log-out/log-out.component.ts
+++ src/app/shared/navbar/log-out/log-out.component.ts
@@ -1,15 +1,32 @@
+import { Dialog, DialogRef } from '@angular/cdk/dialog';
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-out',
   templateUrl: './log-out.component.html',
-  styleUrls: ['./log-out.component.css']
+  styleUrls: ['./log-out.component.scss']
 })
 export class LogOutComponent implements OnInit {
 
-  constructor() { }
+  constructor( public DialogRef: MatDialogRef<LogOutComponent>, private _Router:Router) { }
 
   ngOnInit() {
   }
-
+  onNoClick(): void {
+    this.DialogRef.close();
+    
+  }
+  onLogout(){
+    localStorage.removeItem('userToken');
+    localStorage.removeItem('role');
+    localStorage.removeItem('userName');
+    localStorage.removeItem('userEmail');
+    this._Router.navigate(['/auth']);
+    this.DialogRef.close();
+   
+  }
+  
+  
 }
